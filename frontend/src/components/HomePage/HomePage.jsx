cdb518cac6ad49611bfcc5a355eb548ade08eb62
Quentin Vayssieres <quentin.vayssieres@gmail.com>
2023-11-23 15:51:46 +0100

    feat: add context at homepage

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import "./HomePage.scss";
+import { useOptions } from "../../contexts/PlayerContext";
 
 function HomePage() {
+  const { difficulty, limit, setDifficulty, setLimit } = useOptions();
   const [selectedDifficulty, setSelectedDifficulty] = useState(null);
 
+  // eslint-disable-next-line no-shadow
   const handleDifficultySelect = (difficulty) => {
     setSelectedDifficulty(difficulty);
+    setDifficulty(difficulty);
   };
+  // eslint-disable-next-line no-restricted-syntax
+  console.log(difficulty);
+  const handleLimitChange = (event) => {
+    let newLimit = event.target.value;
+
+    newLimit = Math.min(newLimit, 20);
+
+    setLimit(newLimit);
+  };
+  // eslint-disable-next-line no-restricted-syntax
+  console.log(limit);
 
   return (
     <div className="home-page">
@@ -17,7 +32,14 @@ function HomePage() {
         <div className="text-field-home-page">
           <p>Nombre de participants:</p>
         </div>
-        <input type="text" id="input-field-home-page" name="input" />
+        <input
+          type="text"
+          id="input-field-home-page"
+          name="input"
+          value={limit}
+          onChange={handleLimitChange}
+          max={20}
+        />
       </div>
       <div className="difficulty-home-page">
         <h2>Choisissez la difficulté</h2>
@@ -37,7 +59,7 @@ function HomePage() {
           }`}
           onClick={() => handleDifficultySelect("normal")}
         >
-          Intermédiaire
+          Normal
         </button>
         <button
           type="button"
