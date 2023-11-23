86b0c585372b350d332879be010bf8df2ea6d077
DELMAS <delmastristan64@gmail.com>
2023-11-23 14:59:34 +0100

    chore: add player page created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,14 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import AddPlayerPage from "./pages/AddPlayerPage";
 
 const router = createBrowserRouter([
   {
     element: <App />,
     children: [
       { path: "/", element: <>Home Page</> },
-      { path: "/joueur", element: <>Player</> },
+      { path: "/joueur", element: <AddPlayerPage /> },
       { path: "/roue", element: <>Roue</> },
       { path: "/question", element: <>Question</> },
       { path: "/classement", element: <>Classement</> },
