fd370a44dbb0ccbbf9afb04242e1c5efbbd1dbd9
Quentin Vayssieres <quentin.vayssieres@gmail.com>
2023-11-24 12:09:23 +0100

    fix: timer ConfetiesScripts

--- frontend/src/components/ConfetiesScript.jsx
+++ frontend/src/components/ConfetiesScript.jsx
@@ -1,36 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import confetti from "canvas-confetti";
 
 function ConfettiComponent() {
+  const [isActive, setIsActive] = useState(true);
+
   useEffect(() => {
-    const end = Date.now() + 15 * 1000;
+    if (!isActive) {
+      return;
+    }
+
     const colors = ["#bb0000", "#ffffff"];
 
-    const frame = () => {
+    const launchConfetti = () => {
       confetti({
-        particleCount: 2,
+        particleCount: 100,
         angle: 60,
         spread: 55,
         origin: { x: 0 },
         colors,
       });
       confetti({
-        particleCount: 2,
+        particleCount: 100,
         angle: 120,
         spread: 55,
         origin: { x: 1 },
         colors,
       });
-
-      if (Date.now() < end) {
-        requestAnimationFrame(frame);
-      }
     };
 
-    frame();
+    const confettiInterval = setInterval(() => {
+      launchConfetti();
+    }, 500);
 
-    return () => confetti.reset();
-  }, []);
+    setTimeout(() => {
+      clearInterval(confettiInterval);
+      setIsActive(false);
+    }, 10000);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      clearInterval(confettiInterval);
+      confetti.reset();
+    };
+  }, [isActive]);
 
   return <div className="confetti-container" />;
 }
